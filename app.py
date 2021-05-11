e5eb3ee93db21eaf3fdbb520c544ad4d9cee73b0
Josafá CF <50888435+Josafa01@users.noreply.github.com>
2021-05-11 12:51:12 -0300

    Add files via upload

---
+++ app.py
@@ -0,0 +1,87 @@
+from flask import Flask, render_template, request, redirect, url_for, flash
+from flask_sqlalchemy import SQLAlchemy
+
+
+app = Flask(__name__)
+app.secret_key = "Sd$TUiu!Yd3!"
+
+# Configuração do SqlAlchemy com MYSQL
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql://root:root''@localhost/crud'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+db = SQLAlchemy(app)
+
+
+# Criar um modal para o CRUD
+class Data(db.Model):
+    id = db.Column(db.Integer, primary_key = True)
+    name = db.Column(db.String(100))
+    email = db.Column(db.String(100))
+    phone = db.Column(db.String(100))
+
+
+    def __init__(self, name, email, phone):
+
+        self.name = name
+        self.email = email
+        self.phone = phone
+
+
+# Index com os dados
+@app.route('/')
+def Index():
+    all_data = Data.query.all()
+
+    return render_template("index.html", all_data = all_data)
+
+
+# Rota de insert dos dados
+@app.route('/insert', methods = ['POST'])
+def insert():
+
+    if request.method == 'POST':
+
+        name = request.form['name']
+        email = request.form['email']
+        phone = request.form['phone']
+
+        my_data = Data(name, email, phone)
+        db.session.add(my_data)
+        db.session.commit()
+
+        flash("Cadastro realizado com sucesso")
+
+        return redirect(url_for('Index'))
+
+
+# Rota de update dos dados
+@app.route('/update', methods = ['GET', 'POST'])
+def update():
+
+    if request.method == 'POST':
+        my_data = Data.query.get(request.form.get('id'))
+
+        my_data.name = request.form['name']
+        my_data.email = request.form['email']
+        my_data.phone = request.form['phone']
+
+        db.session.commit()
+        flash("Atualizado com Successfully")
+
+        return redirect(url_for('Index'))
+
+
+# Rota de deletar dados
+@app.route('/delete/<id>/', methods = ['GET', 'POST'])
+def delete(id):
+    my_data = Data.query.get(id)
+    db.session.delete(my_data)
+    db.session.commit()
+    flash("Deletado com Successfully")
+
+    return redirect(url_for('Index'))
+
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
